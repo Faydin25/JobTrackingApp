4ed1b2565acadb5a7d9aa20ed9091854cc03c815
Ferhat Yiğit Aydın <ferhat_yigit_aydin@hotmail.com>
2024-03-21 16:38:58 +0300

    HTML/CSS

---
+++ WebProject/Pages/Register.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+            // GET işlemleri için gereken kodlar
+        }
+
+        public void OnPost()
+        {
+            // POST işlemleri için gereken kodlar
+        }
+    }
+}
