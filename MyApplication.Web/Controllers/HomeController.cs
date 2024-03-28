ed6a5d7fceb0e41783e6fdc08a89792bace22e2e
Ferhat Yiğit Aydın <ferhat_yigit_aydin@hotmail.com>
2024-03-28 15:46:35 +0300

    -

---
+++ MyApplication.Web/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+// using MyApplication.Models; 
+
+namespace MyApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Username, string Password)
+        {
+            // Basit doğrulama
+            if (Username == "admin" && Password == "1234")
+            {
+                // Doğrulama başarılı
+                return RedirectToAction("Profile");
+            }
+            else
+            {
+                // Doğrulama başarısız
+                ViewBag.Error = "Kullanıcı adı veya şifre hatalı!";
+                return View();
+            }
+        }
+
+        public IActionResult Logout() //Profile
+        {
+            // Örneğin, HttpContext.SignOutAsync() kullanılabilir
+            return RedirectToAction("Index");
+        }
+
+    }
+}
